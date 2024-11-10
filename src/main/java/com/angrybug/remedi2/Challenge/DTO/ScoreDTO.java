f68e402ec21f35da0a5218bb47d34fa4a5b0f09c
jihye134seo <jihye.seo134@gmail.com>
2024-11-10 22:02:51 +0900

    edit: user id 타입 변경

--- src/main/java/com/angrybug/remedi2/Challenge/DTO/ScoreDTO.java
+++ src/main/java/com/angrybug/remedi2/Challenge/DTO/ScoreDTO.java
@@ -1,30 +1,17 @@
 package com.angrybug.remedi2.Challenge.DTO;
 
-import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Map;
+import java.util.UUID;
+
+
+@Getter
+@Setter
 public class ScoreDTO {
 
-    private Integer id;
+    private UUID id;
 
     private Map<String, Object> text;
-
-
-    public ScoreDTO() {}
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Map<String, Object> getText() {
-        return text;
-    }
-
-    public void setText(Map<String, Object> text) {
-        this.text = text;
-    }
-
 }
