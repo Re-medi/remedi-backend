6d6ba88b89a09c3013bae25eb2aae7161260aedb
jihye134seo <jihye.seo134@gmail.com>
2024-11-13 00:40:04 +0900

    feat: 백업용 데이터 local 저장 구현

--- src/main/java/com/angrybug/remedi2/DataBackup/Service/DataBackupService.java
+++ src/main/java/com/angrybug/remedi2/DataBackup/Service/DataBackupService.java
@@ -1,15 +1,24 @@
 package com.angrybug.remedi2.DataBackup.Service;
 
+import com.angrybug.remedi2.Challenge.Model.Score;
 import com.angrybug.remedi2.DataBackup.DTO.PracticeResultInputDTO;
 import com.angrybug.remedi2.DataBackup.DTO.PracticeResultOutputDTO;
 import com.angrybug.remedi2.DataBackup.Model.Result;
 import com.angrybug.remedi2.DataBackup.Repository.DataBackupRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
+
+@Slf4j
 @Service
 public class DataBackupService {
 
@@ -38,4 +47,33 @@ public class DataBackupService {
             throw new RuntimeException("Data read failed", e);
         }
     }
+
+
+
+    public void saveLocalScoreDetail(Result savedResult) {
+
+        // 데이터를 Map으로 구성하여 JSON 형식으로 변환
+        Map<String, Object> data = new HashMap<>();
+        data.put("practiceId", savedResult.getPracticeId());
+        data.put("result", savedResult.getResult());
+
+
+        // ObjectMapper를 사용하여 JSON 파일로 저장
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String fileName = savedResult.getPracticeId() + "----" + savedResult.getResult() + ".json";
+
+        String path = "../../../../../backupData/" + fileName;
+
+        try {
+            objectMapper.writeValue(new File(path), data);
+            log.info("{}이 성공적으로 저장되었습니다.", fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
